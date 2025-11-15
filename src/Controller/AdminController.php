fea135ae44bc0de85a9da2c7de6a0c60501efd47
Théo <theo.schaller57@gmail.com>
2025-11-15 11:43:43 +0100

    🚧 Admin dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,8 +18,48 @@ final class AdminController extends AbstractController
             return $this->redirectToRoute('home');
         }
 
-        return $this->render('pages/admin/dashboard.html.twig', [
-            'controller_name' => 'AdminController',
+        return $this->render('pages/admin/index.html.twig');
+    }
+
+    #[Route('/{_locale}/admin/users', name: 'admin.users', requirements: ['_locale' => 'fr|en'])]
+    public function users(UserRepository $userRepository): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('home');
+        }
+
+        $users = $userRepository->findAll();
+
+        return $this->render('pages/admin/users.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/{_locale}/admin/products', name: 'admin.products', requirements: ['_locale' => 'fr|en'])]
+    public function products(ProductRepository $productRepository): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('home');
+        }
+
+        $products = $productRepository->findAll();
+
+        return $this->render('pages/admin/products.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/{_locale}/admin/categories', name: 'admin.categories', requirements: ['_locale' => 'fr|en'])]
+    public function categories(CategoryRepository $categoryRepository): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('home');
+        }
+
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('pages/admin/categories.html.twig', [
+            'categories' => $categories,
         ]);
     }
 }
